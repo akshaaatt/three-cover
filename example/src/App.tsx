2783c1922dcadbae5f2b09c40249bdaf8f9515ed
Akshat Tiwari <tiwariakshat03@gmail.com>
2022-10-27 18:27:05 +0530

    Fix routing in gh-pages

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import './res/css/styles.css';
-import { HashRouter as Router, Routes, Route} from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Home from './pages';
 import ThreeLibraryPage from './pages/library';
 import Slider from './pages/slider';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/library' element={<ThreeLibraryPage/>}/>
-                <Route path='/slider' element={<Slider/>}/>
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/library' element={<ThreeLibraryPage/>}/>
+            <Route path='/slider' element={<Slider/>}/>
+        </Routes>
     );
 }
 
